803ba60f50643ee76e9ee1ac8ef601b28b53afa2
Shiva_Gowtham_Kale <kaleshivagowtham@gmail.com>
2023-02-03 13:09:13 +0530

    Responsive Navbar

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+import Link from 'next/link';
+
+export default function Footer(){
+
+    return (
+        <div className={styles.wholeCont}>
+
+        </div>
+    )
+}
